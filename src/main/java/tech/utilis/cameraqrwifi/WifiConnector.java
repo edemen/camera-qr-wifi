cdd0bf8f305b30431b5785aafcd182a5c8bc5a68
Eugene Dementiev <dementiev.eugene@gmail.com>
2019-06-14 01:33:43 +0100

    Added method for shell execution to String

--- src/main/java/tech/utilis/cameraqrwifi/WifiConnector.java
+++ src/main/java/tech/utilis/cameraqrwifi/WifiConnector.java
@@ -1,5 +1,11 @@
 package tech.utilis.cameraqrwifi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  *
  * @author Eugene Dementiev
@@ -10,5 +16,20 @@ public class WifiConnector {
 		System.out.println("Connecting to network: " + ssid);
 		System.out.println("with password: " + password);
 		throw new UnsupportedOperationException("Not implemented yet");
+	}
+	
+	public static String exec(List<String> args) throws IOException, InterruptedException{
+		ProcessBuilder pb = new ProcessBuilder(args);
+		Process proc = pb.start();
+		BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+		StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+		br.lines().iterator().forEachRemaining(sj::add);
+		proc.waitFor();
+
+		String output = sj.toString();
+		
+		proc.destroy();
+		
+		return output;
 	}
 }
